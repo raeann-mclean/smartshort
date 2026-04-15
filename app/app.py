15ca676336c755914590960b94b1aeea88fd0fb8
raeann-mclean <raeannmcl@aol.com>
2026-04-15 14:23:10 -0400

    change main page

--- app/app.py
+++ app/app.py
@@ -5,4 +5,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hi hi! Lets experiment with making a link shortener + using devops pipeline"
+    return "Hi hi! Lets experiment with making a link shortener w/ flask + use docker for ease of use + SQLite for storage. This is the home page. Try going to /shorten to shorten a link or /links to see all shortened links."
