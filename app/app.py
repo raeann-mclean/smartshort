623c0fd8ec90a18ef62e2df44d3a1618ac029b0a
raeann-mclean <raeannmcl@aol.com>
2026-04-15 18:08:13 -0400

    fix + add routes, base ready

--- app/app.py
+++ app/app.py
@@ -1,8 +1,31 @@
 
 #
-from flask import Flask
+from flask import Flask, redirect, request, jsonify, render_template
+import string
+import random
+
 app = Flask(__name__)
+
+url_map = {}  #dictionary 4 storing
 
 @app.route("/")
 def home():
-    return "Hi hi! Lets experiment with making a link shortener w/ flask + use docker for ease of use + SQLite for storage. This is the home page. Try going to /shorten to shorten a link or /links to see all shortened links."
+    return render_template('home.html')
+
+
+def generate_code(length=6):
+    return ''.join(random.choices(string.ascii_letters + string.digits, k=length))
+
+@app.route('/shorten', methods=['POST'])
+def shorten():
+    data = request.get_json()
+    long_url = data.get('url')
+    if not long_url:
+        return jsonify({'error': 'No URL provided'}), 400
+    code = generate_code()
+    url_map[code] = long_url
+    return jsonify({'short_url': f'http://localhost:5000/{code}'})
+
+@app.route('/links', methods=['GET'])
+def links():
+    return jsonify(url_map)
